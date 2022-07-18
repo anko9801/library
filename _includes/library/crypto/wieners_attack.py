371b28365982b62a4f3ab60a9eef88abae41aab6
anko9801 <daiusa3@icloud.com>
2022-07-18 19:33:02 +0900

    referctoring wiener's attack

--- _includes/library/crypto/wieners_attack.py
+++ _includes/library/crypto/wieners_attack.py
@@ -1,31 +1,46 @@
 import gmpy2
 
 
-def WienersAttack(n, e):
-    r0, r1 = e, n
-    k0, k1 = 0, 1
-    d0, d1 = 1, 0
+def convergents(a, b):
+    r0, r1 = a, b
+    a0, a1 = 0, 1
+    b0, b1 = 1, 0
 
     i = 0
     while r1 != 0:
+        i += 1
         q = r0 // r1
         r0, r1 = r1, r0 % r1
-        k0, k1 = k1, q*k1 + k0
-        d0, d1 = d1, q*d1 + d0
+        a0, a1 = a1, q*a1 + a0
+        b0, b1 = b1, q*b1 + b0
 
         if i % 2 == 0:
-            k = k1 + k0
-            d = d1 + d0
+            a = a1 + a0
+            b = b1 + b0
         else:
-            k = k1
-            d = d1
+            a = a1
+            b = b1
 
-        i += 1
+        yield a, b
+
+
+def has_integer_roots(a, b, c):
+    D = b*b - 4*a*c
+    if D > 0:
+        sD = gmpy2.isqrt(D)
+        if sD * sD == D and (-b + sD) % (2*a) == 0:
+            return 2
+    elif D == 0 and b % (2*a) == 0:
+        return 1
+    return 0
+
+
+def WienersAttack(n, e):
+    for d, k in convergents(n, e):
         if k == 0 or (e * d - 1) % k != 0:
             continue
-        s = n - (e * d - 1) // k + 1
-        D = s*s - 4*n
-        sD = gmpy2.isqrt(D)
-        if D > 0 and sD * sD == D:
+        phi = (e * d - 1) // k
+        s = n - phi + 1
+        if has_integer_roots(1, -s, n) > 0:
             return d
     return -1
